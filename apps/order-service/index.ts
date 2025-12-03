526266e375032665379db127ab73b9a082662d75
zaryab <zaryabkhan248@gmail.com>
2025-12-03 10:29:33 +0500

    order-service initilized with fastify framewrok

---
+++ apps/order-service/index.ts
@@ -0,0 +1,13 @@
+import Fastify from "fastify";
+
+// instance of Fastify
+const fastify = Fastify();
+
+// Running the server!
+try {
+  await fastify.listen({ port: 8001 });
+  console.log("Product service running on port 8001");
+} catch (err) {
+  fastify.log.error(err);
+  process.exit(1);
+}
