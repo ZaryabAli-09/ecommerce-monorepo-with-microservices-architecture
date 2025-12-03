d2e8b30d60837fc2668a086fa8783dc479157f71
zaryab <zaryabkhan248@gmail.com>
2025-12-03 10:45:33 +0500

    payment-service initilized with hono framewrok

--- apps/order-service/index.ts
+++ apps/order-service/index.ts
@@ -6,7 +6,7 @@ const fastify = Fastify();
 // Running the server!
 try {
   await fastify.listen({ port: 8001 });
-  console.log("Product service running on port 8001");
+  console.log("Order service running on port 8001");
 } catch (err) {
   fastify.log.error(err);
   process.exit(1);
---
+++ apps/payment-service/src/index.ts
@@ -0,0 +1,27 @@
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+
+const app = new Hono();
+
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+
+async function start() {
+  try {
+    serve(
+      {
+        fetch: app.fetch,
+        port: 8002,
+      },
+      (info) => {
+        console.log("Payments service running on port", `${info.port}`);
+      }
+    );
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+}
+
+start();
