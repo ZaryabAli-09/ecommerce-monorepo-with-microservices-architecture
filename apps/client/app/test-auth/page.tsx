c94bca1e120df308156c641b514f3fb0c267eb06
zaryab <zaryabkhan248@gmail.com>
2025-12-03 12:52:22 +0500

    clerk tested in product service with express framework

---
+++ apps/client/app/test-auth/page.tsx
@@ -0,0 +1,25 @@
+import { auth } from "@clerk/nextjs/server";
+
+export default async function TestAuth() {
+  const { getToken } = await auth();
+  const token = await getToken();
+
+  async function testAuth() {
+    const res = await fetch("http://localhost:8000/test-auth", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const data = await res.json();
+
+    console.log(res);
+  }
+
+  testAuth();
+
+  return (
+    <div>
+      <button>Testing auth .........</button>
+    </div>
+  );
+}
--- apps/product-service/index.ts
+++ apps/product-service/index.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
+import { clerkMiddleware, getAuth } from "@clerk/express";
+
 const app = express();
 app.use(express.json());
 app.use(
@@ -11,6 +13,19 @@ const port = process.env.PORT || 8000;
 
 app.get("/", (req: Request, res: Response) => {
   res.json("Hello from product service");
+});
+
+app.use(clerkMiddleware());
+
+app.get("/test-auth", (req: Request, res: Response) => {
+  const auth = getAuth(req);
+  const { userId, isAuthenticated } = auth;
+  console.log(auth);
+
+  if (!userId || !isAuthenticated) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+  res.json({ message: "Product service authenticated", data: auth });
 });
 
 app.listen(port, () => {
