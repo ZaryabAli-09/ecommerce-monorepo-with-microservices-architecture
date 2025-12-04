3a6f4eee44350beabfab35f7a1c31bf751c18db6
zaryab <zaryabkhan248@gmail.com>
2025-12-04 10:04:08 +0500

    setuped clerk on fastify service

--- apps/order-service/index.ts
+++ apps/order-service/index.ts
@@ -1,17 +1,37 @@
-import Fastify from "fastify";
+// import 'dotenv/config'
 
+import Fastify from "fastify";
+import { clerkPlugin, getAuth } from "@clerk/fastify";
 // instance of Fastify
 const fastify = Fastify();
+
+fastify.register(clerkPlugin);
 
 fastify.get("/", (request, reply) => {
   return reply.send("Hello from order service");
 });
 
+fastify.get("/test-auth", (request, reply) => {
+  try {
+    const { isAuthenticated, userId } = getAuth(request);
+
+    if (!isAuthenticated) return reply.status(401).send("Not authenticated");
+
+    return reply.send({ user: userId });
+  } catch (error) {
+    fastify.log.error(error);
+  }
+  return reply.send("Hello from order service");
+});
+
 // Running the server!
-try {
-  await fastify.listen({ port: 8001 });
-  console.log("Order service running on port 8001");
-} catch (err) {
-  fastify.log.error(err);
-  process.exit(1);
-}
+const start = async () => {
+  try {
+    await fastify.listen({ port: 8001 });
+  } catch (error) {
+    fastify.log.error(error);
+    process.exit(1);
+  }
+};
+
+start();
